8ef13a195f1ea16edf55a33c450a7ef041a9ff65
HCS <100205514+Hampe007@users.noreply.github.com>
2026-09-15 10:58:32 +0200

    Add chest deposit animation, audio & controller
    Adds Chest_Deposit animation, an AnimatorController for the low-poly (temp) chest, audio clip and audio mixer (plus .meta files). Updates Dropoff_Point prefab to assign the animator controller and set chest rotation. Updates Dropoff_Scene to configure the chest AudioSource (pitch, volume, output mixer) and reference the openSound clip. Updates DropOffChestAnimator to require an AudioSource, expose an openSound field, and play the sound with PlayOneShot when triggering the Deposit animation.

--- Assets/Dropoff/Scripts/DropOffChestAnimator.cs
+++ Assets/Dropoff/Scripts/DropOffChestAnimator.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class DropOffChestAnimator : MonoBehaviour
 {
     private static readonly int DepositTrigger = Animator.StringToHash("Deposit");
 
+    [SerializeField] private AudioClip openSound;
+    
+    private AudioSource chestAudioSource;
     private Animator chestAnimator;
 
-    private void Awake() => chestAnimator = GetComponent<Animator>();
+    private void Awake()
+    {
+        chestAnimator = GetComponent<Animator>();
+        chestAudioSource = GetComponent<AudioSource>();
+    }
+    
+    public void PlayDepositAnimation()
+    {
+        chestAnimator.SetTrigger(DepositTrigger);
 
-    public void PlayDepositAnimation() => chestAnimator.SetTrigger(DepositTrigger);
+        if (openSound != null)
+        {
+            chestAudioSource.PlayOneShot(openSound);
+        }
+    }
 }
