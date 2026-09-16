1f8798c8a4b12ec6dd9c465e2faf3ca811ba8402
HCS <100205514+Hampe007@users.noreply.github.com>
2026-09-16 19:18:23 +0200

    Added anim and audio to chest

--- Assets/Scripts/Interactable/DropOff/DropOffChestAnimator.cs
+++ Assets/Scripts/Interactable/DropOff/DropOffChestAnimator.cs
@@ -1,13 +1,29 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class DropOffChestAnimator : MonoBehaviour
 {
     private static readonly int DepositTrigger = Animator.StringToHash("Deposit");
 
+    [SerializeField] private AudioClip openSound;
+
     private Animator chestAnimator;
+    private AudioSource audioSource;
 
-    private void Awake() => chestAnimator = GetComponent<Animator>();
+    private void Awake()
+    {
+        chestAnimator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
 
-    public void PlayDepositAnimation() => chestAnimator.SetTrigger(DepositTrigger);
+    public void PlayDepositAnimation()
+    {
+        chestAnimator.SetTrigger(DepositTrigger);
+
+        if (openSound != null)
+        {
+            audioSource.PlayOneShot(openSound);
+        }
+    }
 }
