1929dad3c692c709e69bb13ab16a45aee6520d63
HCS <100205514+Hampe007@users.noreply.github.com>
2026-09-18 10:42:16 +0200

    Reverted to the correct CameraLook.cs with small adjustment
    Took back the old CameraLook.cs and added the only part from the latest PR & Merge that was supposed to be added into the script.

--- Assets/Scripts/Player/CameraLook.cs
+++ Assets/Scripts/Player/CameraLook.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -5,38 +6,34 @@ using UnityEngine.InputSystem;
 public class CameraLook : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] InputActionReference look;
+    [SerializeField] InputActionReference Look;
     public Vector3 lookDirection;
     public Vector3 localAngles;
-    private float lookX;
-    private float lookY;
+    float lookX;
+
+    [SerializeField] Rigidbody playerRB;
+
+    Camera cameraComponent;
+    void Start()
+    {
+        cameraComponent = GetComponent<Camera>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+       
         // Do not rotate camera if the game is paused or over
         if (GameState.Instance != null && playerRB != null && !GameState.Instance.IsPlayerInControl())
             return;
+        
+        lookX-=Look.action.ReadValue<Vector2>().y;
 
-        // Read the 2D vector input from the new Input System
-        Vector2 lookInput = look.action.ReadValue<Vector2>();
+        lookX = Mathf.Clamp(lookX,-90, 90);
 
-        // Vertical Look (Pitch): Tilt the camera up and down
-        lookX -= lookInput.y * verticalSensitivity;
-        lookX = Mathf.Clamp(lookX, -90f, 90f);
         transform.localEulerAngles = new Vector3(lookX, 0, 0);
-     
-
-        // Horizontal Look (Yaw): Rotate the player body left and right
-        lookY = lookInput.x * horizontalSensitivity;
-        Vector3 rotationDelta = new Vector3(0, lookY, 0);
-        Quaternion newRotation = playerRB.rotation * Quaternion.Euler(rotationDelta);
-        playerRB.MoveRotation(newRotation);
-
-        // Camera Field of View based on velocity
-        /*if (playerRB != null) {
-            Vector3 flatVelocity = new Vector3(playerRB.linearVelocity.x, 0, playerRB.linearVelocity.z);
-            cameraComponent.fieldOfView = 60 + flatVelocity.magnitude;
-        }*/
+        Vector3 flatVelocity = new Vector3(playerRB.linearVelocity.x, 0, playerRB.linearVelocity.z);
+        cameraComponent.fieldOfView = 60 + flatVelocity.magnitude;
+        
     }
 }
