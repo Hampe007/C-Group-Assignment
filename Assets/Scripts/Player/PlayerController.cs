792a3798d96733fdb7a80a3e0bf7e97297784a05
wh1191 <sebastianvalck@gmail.com>
2026-09-20 14:33:33 +0200

    added PlayerController class to handle most inputs from the player

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    private PlayerMovement movement;
+    private CameraLook cameraLook;
+    private PlayerInteraction playerInteraction;
+
+    [SerializeField] InputActionReference move;
+    [SerializeField] InputActionReference jump;
+    [SerializeField] InputActionReference run;
+    [SerializeField] InputActionReference slide;
+    [SerializeField] InputActionReference look;
+    [SerializeField] InputActionReference interact;
+
+    Vector2 moveInput;
+    Vector2 lookInput;
+    bool isRunning;
+    bool isSliding;
+
+    private void Awake() {
+        movement = GetComponent<PlayerMovement>();
+        cameraLook = GetComponentInChildren<CameraLook>();
+        playerInteraction = GetComponent<PlayerInteraction>();
+    }
+
+    private void OnEnable() {
+        move.action.Enable();
+        jump.action.Enable();
+        run.action.Enable();
+        slide.action.Enable();
+        look.action.Enable();
+        interact.action.Enable();
+
+        jump.action.performed += OnJump;
+        interact.action.performed += OnInteract;
+    }
+
+    private void OnDisable() {
+        jump.action.performed -= OnJump;
+        interact.action.performed -= OnInteract;
+
+        move.action.Disable();
+        jump.action.Disable();
+        run.action.Disable();
+        slide.action.Disable();
+        look.action.Disable();
+        interact.action.Disable();
+    }
+
+    private void Update() {
+        if (movement == null || cameraLook == null || playerInteraction == null) { return; }
+
+        if (GameState.Instance != null && !GameState.Instance.IsPlayerInControl()) {
+            movement.SetInputValues(Vector2.zero, false, false);
+            cameraLook.SetLookInput(Vector2.zero);
+            return;
+        }
+
+        // Read continuous inputs every frame
+        moveInput = move.action.ReadValue<Vector2>();
+        lookInput = look.action.ReadValue<Vector2>();
+        isRunning = run.action.phase == InputActionPhase.Performed;
+        isSliding = slide.action.phase == InputActionPhase.Performed;
+
+        //Debug.Log($"moveInput: {moveInput}, isRunning: {isRunning}, isSliding: {isSliding}");
+
+        // Pass continuous state down
+        movement.SetInputValues(moveInput, isRunning, isSliding);
+        cameraLook.SetLookInput(lookInput);
+    }
+
+    private void OnJump(InputAction.CallbackContext context) {
+        if (movement == null)
+            return;
+
+        if (GameState.Instance != null && !GameState.Instance.IsPlayerInControl())
+            return;
+
+        movement.TriggerJump();
+    }
+
+    private void OnInteract(InputAction.CallbackContext context) {
+        if (playerInteraction == null)
+            return;
+
+        if (GameState.Instance != null && !GameState.Instance.IsPlayerInControl())
+            return;
+
+        playerInteraction.TriggerInteract();
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,11 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public enum PlayerState
 {
-    ground = 0,
-    air = 1,
-    sliding = 2,
-    slideJump = 3
+    GROUNDED = 0,
+    AIRBORNE = 1,
+    SLIDING = 2,
+    SLIDE_JUMP = 3
 }
 
 [RequireComponent(typeof(Rigidbody))]
@@ -17,55 +15,44 @@ public class PlayerMovement : MonoBehaviour
 
     CapsuleCollider capsuleCollider;
 
-    [Header("Movement")]
-    [SerializeField] float acceleration;
-    [SerializeField] float walkMaxSpeed;
-    [SerializeField] float runMaxSpeed;
-    [SerializeField] float gravityScale;
-    [SerializeField] float jumpStrength;
-    [SerializeField] float groundDrag;
-    [SerializeField] float airDrag;
-    [SerializeField] float slopeDrag;
-    [SerializeField] float airMultiplier;
-    [SerializeField] float maxSlopeAngle;
-    [SerializeField] float slopeAcceleration;
+    [Header("Movement Settings")]
+    [SerializeField] private float acceleration;
+    [SerializeField] private float walkMaxSpeed;
+    [SerializeField] private float runMaxSpeed;
+    [SerializeField] private float gravityScale;
+    [SerializeField] private float jumpStrength;
+    [SerializeField] private float groundDrag;
+    [SerializeField] private float airDrag;
+    [SerializeField] private float slopeDrag;
+    [SerializeField] private float airMultiplier;
+    [SerializeField] private float maxSlopeAngle;
+    [SerializeField] private float slopeAcceleration;
 
-    [SerializeField] int maxAirJumps;
-    int airJumps;
+    [SerializeField] private int maxAirJumps;
+    private int airJumps;
+    private float maxSpeed;
 
-    float maxSpeed;
-
-    [Header("Input Actions")]
-    [SerializeField] InputActionReference move;
-    [SerializeField] InputActionReference jump;
-    [SerializeField] InputActionReference run;
-    [SerializeField] InputActionReference slide;
+    //[Header("Input Actions")]
+    //[SerializeField] InputActionReference move;
+    //[SerializeField] InputActionReference jump;
+    //[SerializeField] InputActionReference run;
+    //[SerializeField] InputActionReference slide;
 
     [Header("Ground Check")]
-    [SerializeField] LayerMask groundLayerMask;
-    [SerializeField] float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] private float groundCheckDistance;
+
+    // State passed from PlayerController
+    private Vector2 moveInput;
+    private bool isRunPressed;
+    private bool isSlidePressed;
 
     PlayerState currentState;
-
-    Vector2 moveInput;
-    Vector3 moveDirection;
-    bool isClimbing;
+    private Vector3 moveDirection;
+    private bool isClimbing;
 
     RaycastHit slopeHit;
     float slopeAngle;
-
-    bool IsOnSlope()
-    {
-        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, groundCheckDistance, groundLayerMask))
-        {
-            slopeAngle = Vector3.Angle(Vector3.up, slopeHit.normal);
-
-            if (slopeAngle < maxSlopeAngle && slopeAngle != 0)
-                return true;
-        }
-
-        return false;
-    }
 
     private void Awake()
     {
@@ -79,40 +66,26 @@ public class PlayerMovement : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    private void OnEnable()
-    {
-        print("enable");
+    // Called continuously by PlayerController to update movement inputs.
+    public void SetInputValues(Vector2 inputVector, bool running, bool sliding) {
+        moveInput = inputVector;
+        isRunPressed = running;
+        isSlidePressed = sliding;
 
-        move.action.Enable();
-        jump.action.Enable();
-        run.action.Enable();
-        slide.action.Enable();
-
-        jump.action.performed += OnJump;
+        //Debug.Log($"moveInput: {moveInput}, isRunning: {isRunPressed}, isSliding: {isSlidePressed}");
     }
 
-    private void OnDisable()
-    {
-        print("disable");
-
-        jump.action.performed -= OnJump;
-
-        move.action.Disable();
-        jump.action.Disable();
-        run.action.Disable();
-        slide.action.Disable();
-    }
-
-    private void Update()
-    {
-        if (GameState.Instance != null && !GameState.Instance.IsPlayerInControl())
+    public void TriggerJump() {
+        if (airJumps <= 0 && (currentState == PlayerState.AIRBORNE || currentState == PlayerState.SLIDE_JUMP))
             return;
 
-        moveInput = move.action.ReadValue<Vector2>();
+        if (currentState == PlayerState.AIRBORNE || currentState == PlayerState.SLIDE_JUMP)
+            airJumps--;
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpStrength, rb.linearVelocity.z);
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         if (GameState.Instance != null && !GameState.Instance.IsPlayerInControl())
             return;
         
@@ -123,13 +96,22 @@ public class PlayerMovement : MonoBehaviour
         SpeedControl();
     }
 
-    bool CanExitSlide()
-    {
+    bool IsOnSlope() {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, groundCheckDistance, groundLayerMask)) {
+            slopeAngle = Vector3.Angle(Vector3.up, slopeHit.normal);
+
+            if (slopeAngle < maxSlopeAngle && slopeAngle != 0)
+                return true;
+        }
+
+        return false;
+    }
+
+    bool CanExitSlide() {
         return !Physics.Raycast(transform.position, Vector3.up, groundCheckDistance, groundLayerMask);
     }
 
-    void SetPlayerState()
-    {
+    void SetPlayerState() {
         Vector3 flatVel = rb.linearVelocity;
         flatVel.y = 0;
 
@@ -139,27 +121,29 @@ public class PlayerMovement : MonoBehaviour
             groundCheckDistance,
             groundLayerMask);
 
-        bool slidePressed = slide.action.phase == InputActionPhase.Performed;
+        //bool slidePressed = slide.action.phase == InputActionPhase.Performed;
 
         if (isGrounded)
         {
-            if (slidePressed)
+            /*if (slidePressed)
             {
-                currentState = PlayerState.sliding;
+                currentState = PlayerState.SLIDING;
             }
             else
             {
-                currentState = PlayerState.ground;
-            }
+                currentState = PlayerState.GROUNDED;
+            }*/
+
+            currentState = isSlidePressed ? PlayerState.SLIDING : PlayerState.GROUNDED;
         }
-        else if ((currentState == PlayerState.sliding || currentState == PlayerState.slideJump) &&
+        else if ((currentState == PlayerState.SLIDING || currentState == PlayerState.SLIDE_JUMP) &&
                  flatVel.magnitude > runMaxSpeed)
         {
-            currentState = PlayerState.slideJump;
+            currentState = PlayerState.SLIDE_JUMP;
         }
         else
         {
-            currentState = PlayerState.air;
+            currentState = PlayerState.AIRBORNE;
         }
     }
 
@@ -167,7 +151,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Transform cameraPos = GetComponentInChildren<Camera>().transform;
 
-        if (currentState == PlayerState.sliding)
+        if (currentState == PlayerState.SLIDING)
         {
             capsuleCollider.height = 1;
             capsuleCollider.center = new Vector3(0, -0.5f, 0);
@@ -186,26 +170,32 @@ public class PlayerMovement : MonoBehaviour
         SetPlayerState();
         SetColliderSize();
 
-        maxSpeed = run.action.phase == InputActionPhase.Performed
+        /*maxSpeed = run.action.phase == InputActionPhase.Performed
             ? runMaxSpeed
-            : walkMaxSpeed;
+            : walkMaxSpeed;*/
 
-        switch (currentState)
-        {
-            case PlayerState.ground:
+        maxSpeed = isRunPressed ? runMaxSpeed : walkMaxSpeed;
+
+        switch (currentState) {
+            case PlayerState.GROUNDED:
                 rb.linearDamping = groundDrag;
                 airJumps = maxAirJumps;
                 break;
 
-            case PlayerState.air:
-            case PlayerState.slideJump:
+            case PlayerState.AIRBORNE:
+                break;
+
+            case PlayerState.SLIDE_JUMP:
                 rb.linearDamping = airDrag;
                 rb.linearVelocity += Vector3.down * gravityScale;
                 break;
 
-            case PlayerState.sliding:
+            case PlayerState.SLIDING:
                 rb.linearDamping = slopeDrag;
                 airJumps = maxAirJumps;
+                break;
+
+            default:
                 break;
         }
 
@@ -213,34 +203,40 @@ public class PlayerMovement : MonoBehaviour
         Vector3 right = transform.right * moveInput.x;
         moveDirection = (forward + right).normalized;
 
-        if (currentState == PlayerState.sliding)
+        if (currentState == PlayerState.SLIDING)
             moveDirection = Vector3.zero;
 
-        float multiplier = currentState != PlayerState.air
-            ? 1
-            : airMultiplier;
+        float multiplier = currentState != PlayerState.AIRBORNE ? 1 : airMultiplier;
 
         Vector3 flatVel = rb.linearVelocity;
         flatVel.y = 0;
 
-        if (flatVel.magnitude > maxSpeed && currentState == PlayerState.slideJump)
+        if (flatVel.magnitude > maxSpeed && currentState == PlayerState.SLIDE_JUMP)
             multiplier = .3f;
 
-        Vector3 targetVelocity = moveDirection * acceleration;
+        if(moveDirection.sqrMagnitude > 0.01f) {
+            Vector3 targetVelocity = moveDirection * acceleration;
 
-        if (IsOnSlope())
-        {
-            if (currentState == PlayerState.sliding)
-                targetVelocity += SlopeAcceleration();
-            else
-                targetVelocity = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized * acceleration;
-        }
-
-        rb.linearVelocity += targetVelocity * multiplier;
+            if (IsOnSlope()) {
+                if (currentState == PlayerState.SLIDING) {
+                    targetVelocity += SlopeAcceleration();
+                } else {
+                    targetVelocity = Vector3.ProjectOnPlane(moveDirection,
+                        slopeHit.normal).normalized * acceleration;
+                }
+            }
+            
+            rb.linearVelocity += targetVelocity * multiplier;
+        } /*else if (currentState == PlayerState.GROUNDED) {
+            // Actively decelerate flat velocity when no keys are pressed
+            flatVel = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+            flatVel = Vector3.Lerp(flatVel, Vector3.zero, groundDrag * Time.fixedDeltaTime);
+            rb.linearVelocity = new Vector3(flatVel.x, rb.linearVelocity.y, flatVel.z);
+        }*/
+        //Debug.Log($"rb.linearVelocity: {rb.linearVelocity}");
     }
 
-    Vector3 SlopeAcceleration()
-    {
+    Vector3 SlopeAcceleration() {
         float angleMultiplier = Mathf.Sin(slopeAngle * Mathf.Deg2Rad);
 
         return Vector3.ProjectOnPlane(Vector3.down, slopeHit.normal).normalized
@@ -248,15 +244,11 @@ public class PlayerMovement : MonoBehaviour
                * angleMultiplier;
     }
 
-    void SpeedControl()
-    {
-        if (currentState == PlayerState.sliding || currentState == PlayerState.slideJump)
+    void SpeedControl() {
+        if (currentState == PlayerState.SLIDING || currentState == PlayerState.SLIDE_JUMP)
             return;
 
-        Vector3 flatVelocity = new Vector3(
-            rb.linearVelocity.x,
-            0,
-            rb.linearVelocity.z);
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
 
         if (flatVelocity.magnitude > maxSpeed)
         {
@@ -268,24 +260,8 @@ public class PlayerMovement : MonoBehaviour
                 flatVelocity.z);
         }
     }
-
-    private void OnJump(InputAction.CallbackContext context)
-    {
-        if (airJumps <= 0 &&
-            (currentState == PlayerState.air || currentState == PlayerState.slideJump))
-            return;
-
-        if (currentState == PlayerState.air || currentState == PlayerState.slideJump)
-            airJumps--;
-
-        rb.linearVelocity = new Vector3(
-            rb.linearVelocity.x,
-            jumpStrength,
-            rb.linearVelocity.z);
-    }
       
-    public void SetClimbing(bool climbing)
-    {
+    public void SetClimbing(bool climbing) {
         isClimbing = climbing;
     }
 
